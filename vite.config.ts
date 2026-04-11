c8c2aff09f01ebf662661953efd46244db654a66
Girisha P <pgirisha77@gmail.com>
2026-04-11 05:32:09 +0530

    remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gen-stack/',
   server: {
     port: 3000,
     open: true
